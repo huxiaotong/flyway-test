6d011c23fe051c1a8514db9dea8fe9cc1b29b2e9
huxiaotong <huxiaotong@msn.com>
2020-07-31 18:00:41 +0900

    add

--- sql/V1.01__create_schema.sql
+++ sql/V1.01__create_schema.sql
@@ -1,6 +1,6 @@
-create role scmadmin with login password 'scmadmin';
-create role scmuser with login password 'scmuser';
-create role scmview with login password 'scmview';
+create role if not exists scmadmin with login password 'scmadmin';
+create role if not exists scmuser with login password 'scmuser';
+create role if not exists scmview with login password 'scmview';
 
 create schema if not exists scm authorization scmadmin;
 
