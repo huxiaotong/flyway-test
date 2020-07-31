a665de692d571a7f6bf21930719259a11d2fdd47
huxiaotong <huxiaotong@msn.com>
2020-07-31 18:07:48 +0900

    add

--- sql/V1.01__create_schema.sql
+++ sql/V1.01__create_schema.sql
@@ -1,20 +1,6 @@
-IF NOT EXISTS (
-    SELECT FROM pg_catalog.pg_roles
-    WHERE  rolname = 'scmadmin') THEN
-    create role scmadmin with login password 'scmadmin';
-END IF;
-
-IF NOT EXISTS (
-    SELECT FROM pg_catalog.pg_roles
-    WHERE  rolname = 'scmuser') THEN
-    create role scmuser with login password 'scmuser';
-END IF;
-
-IF NOT EXISTS (
-    SELECT FROM pg_catalog.pg_roles
-    WHERE  rolname = 'scmview') THEN
-    create role scmview with login password 'scmview';
-END IF;
+--create role scmadmin with login password 'scmadmin';
+--create role scmuser with login password 'scmuser';
+--create role scmview with login password 'scmview';
 
 create schema if not exists scm authorization scmadmin;
 
